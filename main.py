61fef9ffeab3f68d5657760be7bc337036c2f094
srikanth-srinivas <srikanth.2001@outlook.com>
2024-04-26 14:49:48 +0530

    added structures.py to store nucleotides as a file

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ randDNAStr = ''.join([random.choice(Nucleotides)
                       for nuc in range(50)])
 
 DNAStr = validateSeq(randDNAStr)
-print(countNucFrequency(DNAStr))
-print(transcription(DNAStr))
+# final print statement with consolidated output 
+print(f'\nSequence: {(DNAStr)}\n')
+print(f'[1] + Sequence Length: {len(DNAStr)}\n')
+print((f'[2] + Nucleotide Frequency: {countNucFrequency(DNAStr)}\n'))
