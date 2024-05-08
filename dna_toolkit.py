b4de9563f2ed5781fbae92ac7d68ac7413694b3b
srikanth-srinivas <srikanth.2001@outlook.com>
2024-05-08 14:10:05 +0530

    added translation and codon usage calculator features

--- dna_toolkit.py
+++ dna_toolkit.py
@@ -42,3 +42,21 @@ def gc_content_subsec(seq, k=20):
         subseq = seq[i:i + k]
         res.append(gc_content(subseq))
     return res
+
+# to translate DNA string into proteins using the codon table
+def translate_seq(seq, init_pos=0):
+    """Translates a DNA sequence into an aminoacid sequence"""
+    return [DNA_Codons[seq[pos:pos + 3]] for pos in range(init_pos, len(seq) - 2, 3)]
+
+def codon_usage(seq, aminoacid):
+    """Provides the frequency of each codon encoding a given aminoacid in a DNA sequence"""
+    tmpList = []
+    for i in range(0, len(seq) - 2, 3):
+        if DNA_Codons[seq[i:i + 3]] == aminoacid:
+            tmpList.append(seq[i:i + 3])
+
+    freqDict = dict(Counter(tmpList))
+    totalWight = sum(freqDict.values())
+    for seq in freqDict:
+        freqDict[seq] = round(freqDict[seq] / totalWight, 2)
+    return freqDict
--- main.py
+++ main.py
@@ -24,3 +24,9 @@ print(f"3' {(colored(reverse_complement(DNAStr)))} 5'\n")
 print(f'[5] + GC Content: {gc_content(DNAStr)}%\n')
 print(
     f'[6] + GC Content in Subsection k=5: {gc_content_subsec(DNAStr, k=5)}\n')
+
+print(
+    f'[7] + Aminoacids Sequence from DNA: {translate_seq(DNAStr, 0)}\n')
+
+print(
+    f'[8] + Codon frequency (L): {codon_usage(DNAStr, "L")}\n')
